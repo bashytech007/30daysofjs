92a1b9ae8d506cc8d830df98293439b29fff6949
bashytech007 <bashiraremu474@gmail.com>
2023-07-19 18:36:46 +0100

    first-Assignment-commit

---
+++ datatypes.js
@@ -0,0 +1,31 @@
+//5
+const myName='bashir'
+const ourBoolean=true
+let imUndefined;
+const imNull='';
+//6
+console.log(typeof myName)
+console.log(typeof ourBoolean)
+console.log(typeof imUndefined)
+console.log(typeof imNull)
+//7
+let firstUndeclaredVariable;
+let secondUndeclaredVariable;
+let thirdUndeclaredVariable;
+let fourthUndeclaredVariable;
+//8
+let firstDeclaredVariable;
+let secondDeclaredVariable;
+let thirdDeclaredVariable;
+let fourthDeclaredVariable;
+
+let firstName='Bashir'
+let lastName='Aremu'
+let maritalStatus='single'
+let country='Nigeria'
+let age='32'
+
+const $firstName = "Bashir", $lastName = "Aremu",$maritalStatus = "single",$country = "Nigeria",$age = "32";
+
+const myAge='I am 25 years old'
+const yourAge='You are 30 years old'
---
+++ variable.js
@@ -0,0 +1,11 @@
+//1 comments can make code readable
+//2 Welcome to 30 days of javascript
+/*comments can make code readable,
+easy to reuse and informative*/
+//4
+const myName='bashir'
+const ourBoolean=1
+let imUndefined;
+const  imNull=null;
+
+
