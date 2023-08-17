91582a9b0e4312dde5ca02711466251f5731f4f2
bashytech007 <bashiraremu474@gmail.com>
2023-08-17 20:42:23 +0100

    week4ass

---
+++ week4/Excercise1.js
@@ -0,0 +1,10 @@
+let a=4;
+let b=3;
+
+if(a>b){
+  console.log('a is greater than b')
+}else if(a<b){
+  console.log('a is lesser than b')
+}
+
+(a>b)? console.log('a is greater than b'): console.log('a is less than or equal to b');
